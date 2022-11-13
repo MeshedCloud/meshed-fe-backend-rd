257a158a35caf240f4bf294d6088162b6b3e44fe
Vincent Vic <vic.coding@gmail.com>
2022-11-13 16:07:19 +0800

    feat: 注册为微应用

--- src/app.tsx
+++ src/app.tsx
@@ -127,3 +127,18 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
 export const request = {
   ...errorConfig,
 };
+
+export const qiankun = {
+  // 应用加载之前
+  async bootstrap(props: any) {
+    console.log('rd bootstrap', props);
+  },
+  // 应用 render 之前触发
+  async mount(props: any) {
+    console.log('rd mount', props);
+  },
+  // 应用卸载之后触发
+  async unmount(props: any) {
+    console.log('rd unmount', props);
+  },
+};
