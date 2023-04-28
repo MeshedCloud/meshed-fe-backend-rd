1483442f12b4ed32073541da3a1bfb4fb072a557
Vincent Vic <vic.coding@gmail.com>
2023-04-28 14:57:23 +0800

    feat: 版本信息更新

--- src/services/deployment/version.ts
+++ src/services/deployment/version.ts
@@ -1,8 +1,9 @@
 export class Version {
   id!: number;
-  name: number | undefined;
-  enname: number | undefined;
-  version: string | undefined;
+  sourceId!: string;
+  name: string | undefined;
+  environments: string[] | undefined;
+  version: number | undefined;
   type!: string;
   status!: string;
 }
@@ -38,6 +39,46 @@ export const VersionTypes = {
   PAGE: {
     color: '#87d068',
     text: '页面'
+  },
+}
+export const VersionEnvironment = {
+  SNAPSHOT: {
+    color: '#2db7f5',
+    text: '快照'
+  },
+  RELEASE: {
+    color: '#87d068',
+    text: '正式'
+  },
+}
+export const VersionStatus = {
+  SUBMIT: {
+    color: '#2db7f5',
+    text: '制品'
+  },
+  BUILDING: {
+    color: '#b9d068',
+    text: '构建中'
+  },
+  BUILD: {
+    color: '#68d09c',
+    text: '构建完成'
+  },
+  BUILD_FAILED: {
+    color: '#d91818',
+    text: '构建失败'
+  },
+  DEPLOYMENT_FAILED: {
+    color: '#d91818',
+    text: '部署失败'
+  },
+  PUBLISHED: {
+    color: '#87d068',
+    text: '已发布'
+  },
+  REJECT: {
+    color: '#e15d5d',
+    text: '拒绝'
   },
 }
 
